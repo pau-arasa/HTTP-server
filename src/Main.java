f2b51cf6825a1b19ed59749877ae47fe7718bb69
pau-arasa <pau.arasa@students.salle.url.edu>
2025-08-14 22:47:07 +0200

    Initial setup of the HTTP server.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Program started");
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            serverSocket.setReuseAddress(true);
 
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Accepted connection: listening on port 8080");
+        } catch (IOException e) {
+            System.out.println("IOException:" +  e.getMessage());
         }
     }
 }
